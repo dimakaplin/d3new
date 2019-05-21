40d1b6b494f1c8635500df681d64f0988f83039d
DimaKaplin <dimakaplin.pr@gmail.com>
2019-05-21 11:33:00 +0300

    nbrths

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import * as d3 from "d3";
 const dataCustom = {
   name: "ExBuild",
   steps: [
-      {date: "2017-11-17", step: "монтаж"},
-      {date: "2018-11-18", step: "монтаж"},
-      {date: "2019-11-19", step: "монтаж"},
-      {date: "2020-11-20", step: "монтаж"},
-      {date: "2021-11-21", step: "монтаж"},
-      {date: "2022-11-22", step: "монтаж"},
-      {date: "2023-11-23", step: "монтаж"},
-      {date: "2024-11-24", step: "монтаж"},
+      {date: "2017-11-17", step: "монтаж1"},
+      {date: "2018-11-18", step: "монтаж2"},
+      {date: "2019-05-19", step: "монтаж3"},
+      {date: "2020-11-20", step: "монтаж4"},
+      {date: "2021-11-21", step: "монтаж5"},
+      {date: "2022-3-22", step: "монтаж6"},
+      {date: "2023-11-23", step: "монтаж7"},
+      {date: "2024-05-24", step: "монтаж8"},
   ]
 };
 
@@ -33,14 +33,16 @@ class App extends Component {
   createChart = () => {
     let data = this.dataBuilder();
 
-    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+   // console.log(data.steps[0].date);
+
+    let margin = {top: 10, right: 30, bottom: 30, left: 60},
         width = 1200 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
     let svg = d3.select("#info")
         .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", 1500)
+        .attr("height", 500)
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
@@ -49,17 +51,39 @@ class App extends Component {
      return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.step }
      });
 
+    //data.unshift()
+    let startDate = new Date();
+    startDate.setDate(1);
+    startDate.setMonth(0);
+    startDate.setFullYear(data[0].date.getFullYear());
+    console.log(startDate);
+
+      let lastDate = new Date();
+      lastDate.setDate(31);
+      lastDate.setMonth(11);
+      lastDate.setFullYear(data[data.length-1].date.getFullYear());
+      console.log(lastDate);
+
+    data.push({date: lastDate});
+    data.unshift({date: startDate});
+
     console.log(data);
+
     let x = d3.scaleTime()
         .domain(d3.extent(data, function(d) { return d.date; }))
         .range([ 0, width ]);
+
+      let x1 = d3.scaleTime()
+          .domain(d3.extent(data, function(d) { return d.date; }))
+          .range([ x(data[1].date), x(data[data.length-2].date) ]);
+
+      console.log(x(data[data.length-2].date));
 
     let xAxis = svg.append("g")
         .attr("transform", "translate(0,15)")
         .call(d3.axisTop(x));
 
-    let line = svg.append('g')
-        .attr("clip-path", "url(#clip)");
+    let line = svg.append('g');
 
     line.append("path")
         .datum(data)
@@ -68,11 +92,21 @@ class App extends Component {
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("d", d3.line()
-            .x(function(d) { return x(d.date) })
-            .y(function(d) { return 50 })
-        )
+            .x(function(d, i) { return x1(d.date) })
+            .y(function(d, i) { return 50 })
+        );
 
-    svg.selectAll(".plan-tick").data(data).enter().append("rect").attr("class", "plan-tick").attr("x", (d)=> x(d.date)).attr("y", (d)=> 50)
+    let rectsData = [];
+    rectsData = rectsData.concat(data);
+    rectsData.splice(0,1);
+    rectsData.splice(rectsData.length-1, 1);
+    console.log(rectsData);
+
+    svg.selectAll(".plan-tick").data(rectsData).enter().append("rect").attr("class", "plan-tick").attr("id", (d, i)=> "plan" + i).attr("x", (d)=> x(d.date)).attr("y", (d)=> 50-5)
+        .attr("height", 10)
+        .attr("width", 10)
+        .attr("fill", "blue")
+        .attr("transform", (d) => "rotate(45," + x(d.date) + ", 40)");
 
   };
 
